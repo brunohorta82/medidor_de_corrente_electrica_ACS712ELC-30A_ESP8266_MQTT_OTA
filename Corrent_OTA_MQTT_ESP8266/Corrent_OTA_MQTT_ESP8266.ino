405203c5849cdf03b2d7a9d4d6e9165c4be00746
Bruno Horta <brunohorta@Brunos-MBP.lan>
2017-09-25 00:23:37 +0100

    Melhoramento na formula de medição, no exemplo uso um ACS712 de 20A

--- Corrent_OTA_MQTT_ESP8266/Corrent_OTA_MQTT_ESP8266.ino
+++ Corrent_OTA_MQTT_ESP8266/Corrent_OTA_MQTT_ESP8266.ino
@@ -39,22 +39,39 @@ const char * MQTT_WATTS_TOPIC = "current/messure/watts";
 //MQTT BROKERS GRATUITOS PARA TESTES https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
 const char* MQTT_SERVER = "iot.eclipse.org";
 
+int mVperAmp = 100; // 100 for 20A Module
+double voltage = 0;
+double vRMS = 0;
+double ampsRMS = 0;
+
 WiFiClient wclient;
 PubSubClient client(MQTT_SERVER,1883,wclient);
 
 //CONTROL FLAGS
 bool OTA = false;
 bool OTABegin = false;
-int sensorValueAux = 0;
-float valorSensor = 0;
-float valorCorrente = 0;
-float voltsporUnidade = 0.004887586;// 5%1023 //ADC de 5V para o ESP é necessário fazer circuito extra
-// Consultar o datasheet do ACS712
-float sensibilidade = 0.066;
 
-//Tensao da rede AC 220 Volts  na verdade pode variar entre 220v e 250v
-int tensao = 242;
+long lastMsg = 0;
 
+float getVPP() {
+  float result;
+  int readValue;             
+  int maxValue = 0;         
+  int minValue = 1024;          
+
+  uint32_t start_time = millis();
+  while ((millis() - start_time) < 500) { 
+    readValue = analogRead(SENSOR);
+    if (readValue > maxValue) {
+      maxValue = readValue;
+    }
+    if (readValue < minValue) {
+      minValue = readValue;
+    }
+  }
+  result = ((maxValue - minValue) * 5.0) / 1024.0;
+  return result;
+}
 void setup() {
   Serial.begin(SERIAL_BAUDRATE);
   WiFiManager wifiManager;
@@ -100,53 +117,33 @@ bool checkMqttConnection(){
 }
 
 void readSensorAndPublish(){
-for(int i=10000; i>0; i--){
+    voltage = getVPP();  //voltagem pico a pico
+    vRMS = (voltage / 2.0) * 0.707; //voltage root mean square
+    //0.707 (datasheet ACS712)
+    ampsRMS = (vRMS * 1000) / mVperAmp; //calcula a corrente
+    if (ampsRMS <= 0.09) ampsRMS = 0; //limpa ruido
+
+    Serial.print(ampsRMS, 3);
+    Serial.println(" Amps RMS");
+    client.publish(MQTT_AMPS_TOPIC,String(ampsRMS,3).c_str(),true);
+    float watts = ampsRMS * 240;
+
+    Serial.print(watts);
+    Serial.println(" Watts");
+    //MQTT
+    client.publish(MQTT_WATTS_TOPIC,String(watts,1).c_str(),true);
   
-    /* Leitura do  sensor analogico A0  subtraindo o ajuste 
-     *  pela metade da resolução (1023)vcc/2 */
-    sensorValueAux = (analogRead(SENSOR) -511.5); 
-    // Soma dos quadrados das leituras
-    valorSensor += pow(sensorValueAux,2); 
-    //Importante para não perder a ligação ao Broker MQTT durante as leituras
-    client.loop();
-  }
-
-  // Calculo da média dos quadrados e conversão para Volts
-  valorSensor = (sqrt(valorSensor/ 10000)) * voltsporUnidade; 
-  // Calculo da corrente considerando a sensibilidade do sensor (185 mV por ampere) para o sensor de exemplo
-  valorCorrente = (valorSensor/sensibilidade); 
-
-  /*Tratamento para possivel ruido
-  O ACS712 de 30 Amperes é projetado para fazer leituras
-   de valores altos acima de 0.25 Amperes até 30,
-   por isso é normal ocorrer ruidos de até 0.20A */
-  if(valorCorrente <= 0.098){
-    valorCorrente = 0; 
-  }
-
-  valorSensor =0;
-  
-  //Mostra o valor da corrente
-  Serial.print("Corrente : ");
-  Serial.print(valorCorrente, 3);
-  Serial.print(" A ");
-  //MQTT
-  client.publish(MQTT_AMPS_TOPIC,String(valorCorrente,3).c_str(),true);
-  //Calculo da Potência 
-  Serial.print(" Potencia (Consumo) : ");
-  float watts = valorCorrente * tensao;
-  Serial.print(watts);
-  Serial.println(" Watts ");
-  //MQTT
-  client.publish(MQTT_WATTS_TOPIC,String(watts,1).c_str(),true);
-  //Delay para evitar overflow
 }
 void loop() {
 if (WiFi.status() == WL_CONNECTED) {
     if (checkMqttConnection()){
         
       client.loop();
+      long now = millis();
+    if (now - lastMsg > 5000) {
+      lastMsg = now;  // de 5 em 5 segundos
       readSensorAndPublish();
+      }
       if(OTA){
         if(OTABegin){
           setupOTA();
